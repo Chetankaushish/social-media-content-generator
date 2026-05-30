9bafbc7c475348b70946555d4bda2be48d34dc6e
Chetan Sharma <chetansharma53700@gmail.com>
2026-05-30 18:31:47 +0530

    Update generator.py

--- utils/generator.py
+++ utils/generator.py
@@ -5,7 +5,7 @@ Handles all LangChain + Google Gemini interactions for social media content crea
 
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.schema.output_parser import StrOutputParser
+from langchain_core.output_parsers import StrOutputParser
 import google.generativeai as genai
 
 
