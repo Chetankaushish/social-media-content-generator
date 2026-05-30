b8a795d9bbf082d6af6c41031617aaa0922337c3
Chetan Sharma <chetansharma53700@gmail.com>
2026-05-30 20:27:25 +0530

    Update generator.py

--- utils/generator.py
+++ utils/generator.py
@@ -7,7 +7,6 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 import google.generativeai as genai
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_google_genai import ChatGoogleGenerativeAI
 
 
 # ── Platform configs ──────────────────────────────────────────────────────────
